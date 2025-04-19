bd63a092e64eaad4b78f373a0e5024fc047e7415
Chaitanya Bhardwaj <nonib2004@gmail.com>
2025-04-19 19:04:22 +0530

    treatment

--- BackEnd/models/Treatments.js
+++ BackEnd/models/Treatments.js
@@ -5,33 +5,42 @@ import Employee from "./Employee.js";
 import Patient from "./Patient.js";
 
 const Treatments = sequelize.define("Treatments", {
-	Treatment_ID: {
-		type: DataTypes.INTEGER,
-		allowNull: false,
-		references: {
-			model: Treatment_Details,
-			key: "Treatment_ID",
-		},
-	},
+  Treatment_ID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Treatment_Details,
+      key: "Treatment_ID",
+    },
+  },
 
-	Employee_ID: {
-		type: DataTypes.INTEGER,
-		allowNull: false,
-		references: {
-			model: Employee,
-			key: "Employee_ID",
-		},
-	},
+  Employee_ID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Employee,
+      key: "Employee_ID",
+    },
+  },
 
-	Start_Date_Time: {
-		type: DataTypes.DATE,
-		allowNull: false,
-	},
+  Patient_ID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Patient,
+      key: "Patient_ID",
+    },
+  },
 
-	End_Date_Time: {
-		type: DataTypes.DATE,
-		allowNull: true,
-	},
+  Start_Date_Time: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+
+  End_Date_Time: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
 });
 
 export default Treatments;
